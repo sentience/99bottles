0538f0076f6195e4226efe2509af41369635d966
Kevin Yank <thatguy@kevinyank.com>
2017-06-08 09:13:32 +1000

    Implement test 1

---
+++ lib/bottles.rb
@@ -0,0 +1,9 @@
+# 99 Bottles of OOP
+class Bottles
+  def verse(_num_bottles)
+    <<-VERSE
+99 bottles of beer on the wall, 99 bottles of beer.
+Take one down and pass it around, 98 bottles of beer on the wall.
+VERSE
+  end
+end
